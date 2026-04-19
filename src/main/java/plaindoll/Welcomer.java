3e6a6ac21538368d755274c834e8cf94ff48ce52
Иванов <radiomost@mail.ru>
2026-04-19 17:37:15 +0000

    feat: add sayReply method with hunter keyword

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the wasy";
 	}
+    public String sayReply(){
+        return "Good hunter, you have done well.";
+    }
 }
